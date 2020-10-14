304fa87e9a0cce1a29d05f1a2f93977f1bca9758
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2020-10-14 15:58:26 -0500

    added error handling
    also updated imgs

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ const mainRoutes = require('./routes');
 
 app.use(mainRoutes);
 
-app.use((req, res) => res.send('<h1>Expressis working!'));
+app.use((req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = 'Oh no! Page not found!';
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    console.log(err.message, "Status:", err.status);
+  } else {
+    err.message = 'Looks like something went wrong on the server.';
+    console.log(err.message, "Status:", err.status);
+  }
+  res.send(err.message);
+  
+});
+
 app.listen(3000, () => {
   console.log('The application is running on localhost: 3000! Enjoy!');
 });
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-router.get('/', (req, res) => {
-  
+router.get('/', (req, res, next) => {
   res.render('index', { projects });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/project/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  const project = projects.find( ({ id }) => id === +projectId );
+
+  if (project) {
+    res.render('project', { project });
+  } else {
+    const err = new Error();
+    err.status = 404;
+    err.message = "Looks like the project you requested is not here.";
+    next(err);
+  }
 });
 
 module.exports = router;
